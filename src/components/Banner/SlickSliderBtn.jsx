8183b1513d000a81b8dc5dd148ad21791604c8eb
Hung Dinh Trong <dinhhung18052000@gmail.com>
2021-11-25 22:29:25 +0700

    Change Banner Arrow Color

--- src/components/Banner/SlickSliderBtn.jsx
+++ src/components/Banner/SlickSliderBtn.jsx
@@ -1,7 +1,4 @@
-import {
-  faChevronCircleLeft,
-  faChevronCircleRight,
-} from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 export const SlickPrevArrow = (props) => {
@@ -9,10 +6,10 @@ export const SlickPrevArrow = (props) => {
   return (
     <div className='hidden prevBtn'>
       <FontAwesomeIcon
-        icon={faChevronCircleLeft}
+        icon={faChevronLeft}
         size='4x'
         className='cursor-pointer bg-transparent'
-        color='rgba(0,0,0,0.5)'
+        color='#fa5959'
         onClick={onClick}
       />
     </div>
@@ -24,10 +21,10 @@ export const SlickNextArrow = (props) => {
   return (
     <div className='hidden nextBtn'>
       <FontAwesomeIcon
-        icon={faChevronCircleRight}
+        icon={faChevronRight}
         size='4x'
         className='cursor-pointer bg-transparent'
-        color='rgba(0,0,0,0.5)'
+        color='#fa5959'
         onClick={onClick}
       />
     </div>
